b67e1063362381729f62414d5ff378a8567dd1cb
mamunur03 <mamunurrahman0003@gmail.com>
2023-12-16 04:26:23 +0600

    initialize the project

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+
+//Connect to DB
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to Database!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
+module.exports = app;
